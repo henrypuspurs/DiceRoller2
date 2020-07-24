ad894cb9570dfd28c94558c9d3ca60e9df8ca625
henrypuspurs <henry_puspurs@hotmail.co.uk>
2020-07-24 16:20:21 +0100

    Add project files.

---
+++ DiceRoller.Library/DiceTray.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace DiceRoller.Library
+{
+    public class DiceTray
+    {
+        public int DiceType { get; private set; }
+        public int DiceCount { get; private set; }
+        public VantageType VantageType { get; private set; } = VantageType.NoVantage;
+        public int Bonus { get; private set; }
+        public int[] Rolls { get; private set; }
+
+        public DiceTray(string diceType, string diceCount, string bonus)
+        {
+            Bonus = int.Parse(bonus);
+            DiceCount = int.Parse(diceCount);
+            DiceType = int.Parse(Regex.Replace(diceType, @"d", ""));
+            RollDice();
+        }
+
+        private void RollDice()
+        {
+            Rolls = new int[DiceCount];
+            var r = new Random();
+            for (int i=0; i < DiceCount; i++)
+            {
+                Rolls[i] = r.Next(1, DiceType + 1);
+            }
+        }
+    }
+
+    public enum VantageType
+    {
+        NoVantage,
+        Advantage,
+        Disadvantage
+    }
+}
---
+++ DiceRoller.Library/RollMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiceRoller.Library
+{
+    public class RollMessage
+    {
+        public string RollResult { get; private set; }
+
+        public RollMessage(DiceTray diceTray)
+        {
+            RollResult = $"You rolled {diceTray.DiceCount}d{diceTray.DiceType}+{diceTray.Bonus}\n" +
+                $"Result: {diceTray.Rolls.Sum()}";
+        }
+    }
+}
